8481d3e28c14be03d8b7cb74afa5ca8c19792ec5
Müşerref Selçuk Özdemir <s_05_60@hotmail.com>
2023-01-15 15:26:06 +0300

    alintivegif

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -2,12 +2,17 @@ import { Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
+
 const AdvertWidget = () => {
   const { palette } = useTheme();
   const dark = palette.neutral.dark;
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
-
+  const gifs = ["https://64.media.tumblr.com/68bd674e9f4a2cd0f24f85540fd729f3/29f008c37e133aa5-50/s540x810/9faa37f56662fc6a40fd4ace300e005c32930fd3.gif", 
+  "https://cdn.vox-cdn.com/uploads/chorus_asset/file/4185857/giphy__4_.0.gif", 
+  "https://media.tenor.com/zDs5Vi1gL_YAAAAC/the-truman-show-jim-carrey.gif",
+"https://media.tenor.com/JJ9gLUTMRZsAAAAC/12angry-men-black-and-white.gif"];
+ 
   return (
     <WidgetWrapper>
       <FlexBetween>
@@ -20,7 +25,8 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="https://media.tenor.com/QJNoPeq9WmUAAAAC/netflix-intro-netflix.gif"
+        
+        src={gifs[Math.floor(Math.random() * 4)]}
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
